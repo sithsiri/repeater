487ff92e66b1a4dc22776ea83cb01370d901cd0d
sithsiri <sith@live.com>
2015-12-17 22:21:11 -0600

    Lotsa changes
    I think a release may be appropriate

--- repeater.bat
+++ repeater.bat
@@ -3,27 +3,40 @@
 REM This is a Windows program only!
 
 echo Starting...
+echo Changing color to green
 cls
+color 02
 
 type startup.txt
 echo.
+echo To view the license, copy "type %~dp0\LICENSE" and paste it at the prompt.
 echo.
-echo Hello, %USERNAME%! Welcome to Repeater.
+echo Hello, %USERNAME%! Welcome to Repeater. Press any key to continue, if you wish.
 
-pause
+pause >nul
 cls
+
 echo Initializing...
 echo.
+echo Changing color to default...
+color 0F
+echo Done...
 echo.
 echo Teleporting to user directory...
 cd "C:\Users\%USERNAME%"
 echo Done...
 echo.
 echo Ready. We are now at "C:\Users\%USERNAME%".
-pause
+echo.
 echo Clearing...
 cls
 
+REM Here we are...
+cls
+ver
+echo (c) %DATE:~-4% Microsoft Corporation. All rights reserved.
+echo Repeater is repeating...
+echo.
 :loop
 set /p input="%cd%>"
 %input%
---
+++ toGithub.bat
@@ -0,0 +1,1 @@
+start https://github.com/sithsiri/repeater
