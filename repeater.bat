c51eac3e216fd7a22ac0fdc0a78d68aa1f3d4688
sithsiri <sith@live.com>
2016-10-12 11:45:19 -0500

    fix attempt and major cleanup

--- repeater.bat
+++ repeater.bat
@@ -1,13 +1,11 @@
-@echo off
 setlocal EnableDelayedExpansion
-
+@echo off
 REM Everything with a REM is code that doesn't work properly.
-
 REM This is for Windows Command Prompt.
+
 title Repeater v0.0.1-2
 echo Starting...
 cls
-
 type startup.txt
 echo.
 echo To view the license, copy "type %~dp0LICENSE" and paste it at the prompt.
@@ -16,7 +14,6 @@ echo Hello, %USERNAME%! Welcome to Repeater. Press any key to continue, if you w
 
 pause >nul
 cls
-
 echo Initializing...
 echo.
 echo Teleporting to user directory...
@@ -29,42 +26,12 @@ title /dev/sda1/home/%USERNAME%
 echo.
 echo Clearing...
 cls
-
-:▐start
-cls
 ver
 echo (c) %DATE:~-4% Microsoft Corporation. All rights reserved.
 echo Repeater is repeating...
 echo.
 
-REM set ☺superinput☺=
-
-:▐loop
-
-REM %☺superinput☺%
-
-set /p δinputδ="%cd%>"
-REM if %δinputδ:~0,3%==*r* goto :▐*r*
-if %δinputδ:~0,3%==*r* goto :▐*r*
-%δinputδ%
-
-REM set ☺superinput☺=
-
-goto :▐loop
-
-:▐*r*
-set ☺superinputparse☺=
-cls
-echo You have entered Super Cool Hax
-echo Choose from the options below by typing a number:
-echo.
-echo 1: Go to the directory from which Repeater is running from.
-echo 2: Restart Repeater.
-echo 3: View the licence for Repeater.
-echo Press enter to go back.
-echo.
-set /p ☺superinputparse☺=Super Cool Hax is ready:
-if %☺superinputparse☺%==1 cd %~dp0
-if %☺superinputparse☺%==2 goto :▐start
-if %☺superinputparse☺%==3 type %~dp0\LICENSE
-goto :▐loop
+:loop
+set /p input="%cd%>"
+%input%
+goto :loop
